bdfe9ad226c06466573a020b5e1158985750976a
Joseph <meicai0143@126.com>
2021-05-25 14:37:26 +0800

    datetime pick utc time TZ

--- log/logger.go
+++ log/logger.go
@@ -88,7 +88,7 @@ func BuildEncoder(format string) zapcore.Encoder {
 		EncodeCaller:   zapcore.ShortCallerEncoder,
 	} // zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+		enc.AppendString(t.UTC().Format("2006-01-02T15:04:05.000Z"))
 	}
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	var encoder zapcore.Encoder
